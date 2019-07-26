ea0a6827b032ebbb9f312b008e6c2bfa4fd8ac21
Eriold <dacamoty@gmail.com>
2019-07-26 10:48:54 -0500

    add header cors in routes.js

--- backend-motor-park/api/routes.js
+++ backend-motor-park/api/routes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const vehicles = require("../data/data.json");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", function(req, res) {
   res.json(vehicles.filter(e => e.state === "ON").slice(0, 12));
 });
