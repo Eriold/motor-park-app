51aa563bc47b70720a38f70f488c1ac1f126ad9b
Eriold <dacamoty@gmail.com>
2019-07-26 10:53:19 -0500

    Correction cors routes

--- backend-motor-park/api/routes.js
+++ backend-motor-park/api/routes.js
@@ -3,7 +3,7 @@ const app = express();
 const vehicles = require("../data/data.json");
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
